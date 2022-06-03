fafec85e90669ed2ed9c7f9ea77f3ba32307f6a7
ikjo93 <auddlr100@naver.com>
2022-06-04 02:39:48 +0900

    fix : AccommodationController default 좌표값(서울 시청) 삭제
    * Dan 리뷰에 따라 default 좌표값은 클라이언트마다 니즈가 다를 수 있으므로 이를 클라이언트에서 관리하도록 하는 것이 적합하다고 생각

--- be/src/main/java/codesquad/airbnb/controller/AccommodationController.java
+++ be/src/main/java/codesquad/airbnb/controller/AccommodationController.java
@@ -14,17 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccommodationController {
 
-    /* 사용자 현재 위치 좌표 미 전송 시 '서울 시청 좌표'로 검색 */
-    private static final String DEFAULT_LATITUDE = "126.9808";
-    private static final String DEFAULT_LONGITUDE = "37.5662";
-
     private final AccommodationService accommodationService;
 
     @GetMapping("/api/accommodation/prices")
     public AccommodationPriceListDto prices(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate in,
                                             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate out,
-                                            @RequestParam(defaultValue = DEFAULT_LATITUDE) double latitude,
-                                            @RequestParam(defaultValue = DEFAULT_LONGITUDE) double longitude) {
+                                            @RequestParam double latitude,
+                                            @RequestParam double longitude) {
 
         return accommodationService.getPricesByStayDate(in, out, latitude, longitude);
     }
@@ -35,8 +31,8 @@ public class AccommodationController {
                                                @RequestParam int minimum_money,
                                                @RequestParam int maximum_money,
                                                @RequestParam int personnel,
-                                               @RequestParam(defaultValue = DEFAULT_LATITUDE) double latitude,
-                                               @RequestParam(defaultValue = DEFAULT_LONGITUDE) double longitude) {
+                                               @RequestParam double latitude,
+                                               @RequestParam double longitude) {
 
         return accommodationService.getAccommodationInfoByCriteria(in, out, minimum_money, maximum_money, personnel, latitude, longitude);
     }
