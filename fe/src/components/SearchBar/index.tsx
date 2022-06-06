1812066f0e976b8f8c6db46eb944a0170bfd4997
Cola <bcad1591@gmail.com>
2022-06-06 22:52:20 +0900

    feat: Small Search Bar & Big Search Bar Toggle 구현

--- fe/src/components/SearchBar/index.tsx
+++ fe/src/components/SearchBar/index.tsx
@@ -1,6 +1,7 @@
 import { DatePicker, useDatePickReset, useDatePickGetter } from '@bcad1591/react-date-picker';
 import React, { useState, useRef, useEffect, useMemo } from 'react';
 
+import Gnb from '@/components/Gnb';
 import {
   useInputRangeGetter,
   useInputRangeSetter,
@@ -20,7 +21,50 @@ import { useAccommodation } from '@/contexts/Accommodation';
 
 import * as S from './style';
 
-function SearchBar() {
+interface Props {
+  withSmallSearchBar?: boolean;
+}
+
+const usePopup = (popupInfo) => {
+  const [popupElements, setPopupElements] = useState(
+    popupInfo.map((info) => ({
+      ...info,
+      isOpen: false,
+    })),
+  );
+
+  const openPopupElement = (key) => () => {
+    setPopupElements((prevPopupElements) =>
+      prevPopupElements.map((popupElement) => {
+        if (popupElement.key === key) {
+          return {
+            ...popupElement,
+            isOpen: !popupElement.isOpen,
+          };
+        }
+        return {
+          ...popupElement,
+          isOpen: false,
+        };
+      }),
+    );
+  };
+
+  const closeAllPopups = () => {
+    setPopupElements((prevPopupElements) =>
+      prevPopupElements.map((popupElement) => {
+        return {
+          ...popupElement,
+          isOpen: false,
+        };
+      }),
+    );
+  };
+
+  return { popupElements, openPopupElement, closeAllPopups };
+};
+
+function SearchBar({ withSmallSearchBar = false }: Props) {
   const bigSearchBarRef = useRef<HTMLDivElement>(null);
   const [popups, setPopups] = useState([
     {
@@ -36,9 +80,10 @@ function SearchBar() {
       isOpen: false,
     },
   ]);
+  const [isSmallSearchBarVisible, setIsSmallSearchBarVisible] = useState(false);
 
   const {
-    pickedDateUnits: { firstPickedDateUnit, secondPickedDateUnit },
+    pickedDateUnits: { firstPickedDateUnit, secondPickedDateUnit }, // NOTE
   } = useDatePickGetter();
   const { leftInputValue, rightInputValue } = useInputRangeGetter();
   const { scaleFactor, offset, initialMaxPrice, initialMinPrice } = useAccommodation();
@@ -46,7 +91,7 @@ function SearchBar() {
   const resetPickedDates = useDatePickReset();
 
   const minPrice = leftInputValue * scaleFactor + offset;
-  const maxPrice = rightInputValue * scaleFactor + offset;
+  const maxPrice = rightInputValue * scaleFactor + offset; // NOTE
   const initialLeftInputValue = useMemo(
     () => (initialMinPrice - offset) / scaleFactor,
     [initialMinPrice],
@@ -88,61 +133,83 @@ function SearchBar() {
     );
   };
 
+  const handleClickSmallSearchBar = () => {
+    setIsSmallSearchBarVisible(false);
+  };
+
   useEffect(() => {
     const listener = (e: MouseEvent) => {
       if (bigSearchBarRef.current?.contains(e.target as HTMLElement)) {
         return;
       }
 
+      // 모달창이 모두 닫혀있는 경우
+      if (
+        withSmallSearchBar &&
+        !isSmallSearchBarVisible &&
+        popups.every((popup) => {
+          return !popup.isOpen;
+        })
+      ) {
+        setIsSmallSearchBarVisible((prevState) => !prevState);
+      }
+
+      // 모달창이 어느 하나라도 열려 있는 경우
       closeAllPopups();
     };
 
     document.addEventListener('click', listener);
     return () => document.removeEventListener('click', listener);
-  }, []);
+  }, [popups]);
 
   return (
     <>
-      <SmallSearchBar>
-        <DateButton checkIn={firstPickedDateUnit} checkOut={secondPickedDateUnit} />
-        <Small.Separator />
-        <PriceButton minPrice={minPrice} maxPrice={maxPrice} />
-        <Small.Separator />
-        <GuestsButton adults={1} />
-      </SmallSearchBar>
+      <Gnb padding={withSmallSearchBar ? 24 : 80}>
+        {withSmallSearchBar && isSmallSearchBarVisible && (
+          <SmallSearchBar onClick={handleClickSmallSearchBar}>
+            <DateButton checkIn={firstPickedDateUnit} checkOut={secondPickedDateUnit} />
+            <Small.Separator />
+            <PriceButton minPrice={minPrice} maxPrice={maxPrice} />
+            <Small.Separator />
+            <GuestsButton adults={1} />
+          </SmallSearchBar>
+        )}
+      </Gnb>
 
-      <S.BigSearchBarLayout ref={bigSearchBarRef}>
-        <BigSearchBar
-          buttons={
-            <>
-              <BigDateButton
-                checkIn={firstPickedDateUnit}
-                checkOut={secondPickedDateUnit}
-                reset={(e) => {
-                  e?.stopPropagation();
-                  resetPickedDates();
-                }}
-                onClick={openPopup('datePicker')}
-              />
-              <Big.Separator />
-              <BigPriceButton
-                minPrice={minPrice}
-                maxPrice={maxPrice}
-                onClick={openPopup('pricePicker')}
-                reset={(e) => {
-                  e?.stopPropagation();
-                  resetPrices();
-                }}
-              />
-              <Big.Separator />
-              <BigGuestsButton child={1} />
-            </>
-          }
-          popup={popups.find((element) => {
-            return element.isOpen;
-          })}
-        />
-      </S.BigSearchBarLayout>
+      <S.BigSearchBarWrapper>
+        {!isSmallSearchBarVisible && (
+          <S.BigSearchBarLayout ref={bigSearchBarRef} marginTop={withSmallSearchBar ? 24 : 44}>
+            <BigSearchBar
+              buttons={
+                <>
+                  <BigDateButton
+                    checkIn={firstPickedDateUnit}
+                    checkOut={secondPickedDateUnit}
+                    reset={(e) => {
+                      e?.stopPropagation();
+                      resetPickedDates();
+                    }}
+                    onClick={openPopup('datePicker')}
+                  />
+                  <Big.Separator />
+                  <BigPriceButton
+                    minPrice={minPrice}
+                    maxPrice={maxPrice}
+                    onClick={openPopup('pricePicker')}
+                    reset={(e) => {
+                      e?.stopPropagation();
+                      resetPrices();
+                    }}
+                  />
+                  <Big.Separator />
+                  <BigGuestsButton />
+                </>
+              }
+              popup={popups.find((element) => element.isOpen)}
+            />
+          </S.BigSearchBarLayout>
+        )}
+      </S.BigSearchBarWrapper>
     </>
   );
 }
