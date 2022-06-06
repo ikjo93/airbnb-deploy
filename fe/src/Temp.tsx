a4c496001ce25889c47ba1d01dc17a9e45ef23fd
Cola <bcad1591@gmail.com>
2022-06-06 16:39:12 +0900

    refactor: update

--- fe/src/Temp.tsx
+++ fe/src/Temp.tsx
@@ -5,33 +5,21 @@ import Gnb from '@/components/Gnb';
 import Header from '@/components/Header';
 import PricePicker from '@/components/PricePicker';
 import SearchBar from '@/components/SearchBar';
-import BigSearchBar from '@/components/SearchBar/BigSearchBar';
-import SmallSearchBar from '@/components/SearchBar/SmallSearchBar';
 
 function Temp() {
   return (
     <div style={{ padding: 20, background: '#eee', height: '200vh' }}>
-      <h3>검색창 최종</h3>
-      <SearchBar />
-
-      <h3>작은검색창</h3>
-      <SmallSearchBar />
-      <Separator />
       <DatePickerProvider>
-        <h3>큰검색창</h3>
-        {/*<BigSearchBar />*/}
+        <h3>검색창 최종</h3>
+        <SearchBar />
+
         <Separator />
-
-        <h3>Date Picker</h3>
-
-        <DatePicker disablePreviousDays />
       </DatePickerProvider>
 
       <Separator />
 
-      <Gnb>
-        <SmallSearchBar />
-      </Gnb>
+      {/*<Gnb>*/}
+      {/*</Gnb>*/}
       <Separator />
 
       <h3>
