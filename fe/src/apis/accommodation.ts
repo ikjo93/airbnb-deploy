37c706d583d649505721ab0fc841ac5e8b68652b
Cola <bcad1591@gmail.com>
2022-06-08 21:00:20 +0900

    feat: getPrices 추가

--- fe/src/apis/accommodation.ts
+++ fe/src/apis/accommodation.ts
@@ -1,5 +1,21 @@
 // example: /api/accommodation/prices?in=2022-05-31&out=2022-06-02&latitude=127.55&longitude=37.55
 
+import { fetchData } from '@/apis/helpers';
+
 const base = '/api/accommodation';
 
 export const pricesURL = `${base}/prices`;
+
+export const getPrices = async (params: Params) => {
+  const data = await fetchData<PricesData[]>(pricesURL, params);
+  return data;
+};
+
+interface Params {
+  [key: string]: string;
+}
+
+interface PricesData {
+  price: number;
+  count: number;
+}
