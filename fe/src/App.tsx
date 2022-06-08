e14a4980bf030f5520b7d5441c51ec2fc4a6058b
Cola <bcad1591@gmail.com>
2022-06-09 02:17:52 +0900

    feat: catch블럭 추가

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,20 +1,25 @@
 import { DatePickerProvider } from '@bcad1591/react-date-picker';
 import React, { useEffect } from 'react';
 
-import { useGeoLocationSetter } from '@/contexts/GeoLocation';
+import { useGeoLocationSetter, useGeoLocationGetter } from '@/contexts/GeoLocation';
 import Router from '@/Router';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { getGeoLocation } from '@/utils';
 
 function App() {
   const setGeoLocation = useGeoLocationSetter();
+  const myLocation = useGeoLocationGetter();
 
   useEffect(() => {
-    getGeoLocation().then((position) => {
-      const { latitude, longitude } = position.coords;
-      setGeoLocation({ latitude: String(latitude), longitude: String(longitude) });
-    });
+    getGeoLocation()
+      .then((position) => {
+        const { latitude, longitude } = position.coords;
+        setGeoLocation({ latitude: String(latitude), longitude: String(longitude) });
+      })
+      .catch(() => {});
   }, []);
+
+  useEffect(() => console.log(myLocation));
 
   return (
     <>
