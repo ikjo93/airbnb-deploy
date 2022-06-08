9e706aa830fdcc215218b6e9d67cfccbf4914dcc
Cola <bcad1591@gmail.com>
2022-06-08 20:55:58 +0900

    feat: 임시로 검색버튼에 navigate 기능 추가

--- fe/src/components/SearchBar/BigSearchBar/index.tsx
+++ fe/src/components/SearchBar/BigSearchBar/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { SearchButton } from '@/components/buttons/SearchButton';
 
 import * as S from './style';
 
 function BigSearchBar({ buttons, popup }: { buttons: any; popup: any }) {
+  const navigate = useNavigate();
   return (
     <>
       <S.SearchBarLayer>
         {buttons}
         {/* TODO: 위 세가지 필드중 하나라도 채워져야 검색 버튼 활성화 */}
-        <S.SearchButtonLayer disabled={false}>
+        <S.SearchButtonLayer
+          disabled={false}
+          onClick={() => {
+            navigate('/search?key=value');
+          }}
+        >
           <SearchButton>검색</SearchButton>
         </S.SearchButtonLayer>
       </S.SearchBarLayer>
